c212a12e548b613ccf5ec95e40a7e4d1850e334a
CemenVy <semen.vydrin0108@gmail.com>
2023-08-23 21:52:38 +0400

    Work with logic

--- ContactList/ViewControllers/ContactListViewController.swift
+++ ContactList/ViewControllers/ContactListViewController.swift
@@ -8,25 +8,25 @@
 import UIKit
 
 final class ContactListViewController: UITableViewController {
-    private var personList = Person.getPersonList(from: DataStore())
+    private var person = Person.getPersonList(from: DataStore())
     
     //MARK: - Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let indexPath = tableView.indexPathForSelectedRow else { return }
         let personDetailsVC = segue.destination as? PersonDetailsViewController
-        personDetailsVC?.person = personList[indexPath.row]
+        personDetailsVC?.person = person[indexPath.row]
     }
 }
 
 //MARK: - UITabelViewDataSource
 extension ContactListViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        personList.count
+        person.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let personCell = tableView.dequeueReusableCell(withIdentifier: "personCell", for: indexPath)
-        let person = personList[indexPath.row]
+        let person = person[indexPath.row]
         
         var content = personCell.defaultContentConfiguration()
         content.text = person.fullName
@@ -36,7 +36,7 @@ extension ContactListViewController {
     }
     
     override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
-        let currentPerson = personList.remove(at: sourceIndexPath.row)
-        personList.insert(currentPerson, at: destinationIndexPath.row)
+        let currentPerson = person.remove(at: sourceIndexPath.row)
+        person.insert(currentPerson, at: destinationIndexPath.row)
     }
 }
